54d67d2c2b049ae33d572673aa92aac540ee9a08
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-12-01 20:09:50 +0200

    +

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-const counterElement = document.querySelector('#counter');
-let counter = 0;
+async function getCurrencies() {
 
-setInterval(function () {
-    counter++;
-    counterElement.innerText = counter;
-}, 1000);
+    // 1. Получить данные с сервера
+    const url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+    const response = await fetch(url);
+    const data = await response.json();
 
+    // 2. Отобразить эти данные на странице
+    renderRates(data);
+}
 
-// Старт
+getCurrencies();
 
-// Пауза
+function renderRates(data) {
+    const usdRate = data.Valute.USD.Value.toFixed(2);
+    const eurRate = data.Valute.EUR.Value.toFixed(2);
 
-// Сброс
+    const usdElement = document.querySelector('#usd');
+    const eurElement = document.querySelector('#eur');
+
+    usdElement.innerText = usdRate;
+    eurElement.innerText = eurRate;
+}
